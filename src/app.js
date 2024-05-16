44500eaabb7444ac0850766cbf67021ac1938adb
mehedi <mehedihasana383@gmail.com>
2024-05-17 00:26:50 +0600

    all setup configured

--- src/app.js
+++ src/app.js
@@ -12,4 +12,10 @@ app.use(express.static("public"))
 
 app.use(cookieParser());
 
+//router import
+import userRouter from './routes/user.routes.js';
+
+//routes declaration;
+app.use("/api/v1/users",userRouter)
+
 export {app};
