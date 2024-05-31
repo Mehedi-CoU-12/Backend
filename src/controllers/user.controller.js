361f78031a02ffc7f67ae6bad024fce1b131c290
mehedi <mehedihasana383@gmail.com>
2024-06-01 01:09:16 +0600

    add new routes for user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -218,6 +218,15 @@ const changeCurrentPassword=asyncHandler(async(req,res)=>{
     }
 })
 
+const getCurrentUser = asyncHandler(async(req, res) => {
+    return res.status(200)
+    .json(new ApiResponse(
+        200,
+        req.user,
+        "User fetched successfully"
+    ))
+})
+
 const updateAccountDetails=asyncHandler(async(req,res)=>{
     const {fullName,email}=req.body;
 
@@ -386,4 +395,5 @@ export {
     updateUserCoverImage,
     getUserChannelProfile,
     getWatchHistory,
+    getCurrentUser,
 }
